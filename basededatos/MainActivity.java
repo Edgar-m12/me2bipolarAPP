01725281bef12b77610bdb9c880cdf2786c69041
ORDennisA <89487953+ORDennisA@users.noreply.github.com>
2021-12-04 21:48:27 -0800

    Add files via upload

--- basededatos/MainActivity.java
+++ basededatos/MainActivity.java
@@ -5,26 +5,100 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.ContentValues;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class MainActivity extends AppCompatActivity {
 
-    private EditText et_IdPaciente , et_nombre, et_telefono,et_correo, et_genero, et_pwd;
+    EditText IdPaciente, nombre, telefono, correo, genero, pwd; //nombre de las variables
+    Button btnAgregar, btnConsultar, btnModificar, btnBorrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        et_IdPaciente = (EditText)findViewById(R.id.txt_IdPaciente);
-        et_nombre = (EditText)findViewById(R.id.txt_nombre);
-        et_telefono = (EditText)findViewById(R.id.txt_telefono);
-        et_correo = (EditText)findViewById(R.id.txt_correo);
-        et_genero = (EditText)findViewById(R.id.txt_genero);
-        et_pwd = (EditText)findViewById(R.id.txt_pwd);
+        IdPaciente = (EditText) findViewById(R.id.txt_IdPaciente); //txt son los identificadores de los cuadros
+        nombre = (EditText) findViewById(R.id.txt_nombre);
+        telefono = (EditText) findViewById(R.id.txt_telefono);
+        correo = (EditText) findViewById(R.id.txt_correo);
+        genero = (EditText) findViewById(R.id.txt_genero);
+        pwd = (EditText) findViewById(R.id.txt_pwd);
+        btnAgregar = (Button) findViewById(R.id.btnAgregarUsuario);
+        btnConsultar = (Button) findViewById(R.id.btnConsultarUsuario);
+        btnModificar = (Button) findViewById(R.id.btnModificarUsuario);
+        btnBorrar = (Button) findViewById(R.id.btnBorrarUsuario);
+
+        btnAgregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                agregarUsuario();
+            }
+        });
+
+        btnConsultar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                consultaUsuario();
+            }
+        });
     }
 
+    public Connection conexionBD() {
+        Connection conexion = null;
+        try {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+            Class.forName("net.sourceforge.jtds.jdbc.Driver").newInstance();
+            conexion = DriverManager.getConnection("jdbc:jtds:sqlserver://192.140.1.37;DatabaseName = Developer");
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+        return conexion;
+    }
+
+    public void agregarUsuario() {
+        try {
+            PreparedStatement pat = conexionBD().prepareStatement("Introduce en usuarios ");
+            pat.setString(1, IdPaciente.getText().toString());
+            pat.setString(2, nombre.getText().toString());
+            pat.setString(3, telefono.getText().toString());
+            pat.setString(4, correo.getText().toString());
+            pat.setString(5, genero.getText().toString());
+            pat.setString(6, pwd.getText().toString());
+            Toast.makeText(getApplicationContext(), "Agregado con exito", Toast.LENGTH_SHORT).show();
+        } catch (SQLException e) {
+            Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void consultaUsuario() {
+        try {
+            Statement stm = conexionBD().createStatement();
+            ResultSet rs = stm.executeQuery("SELECT*FROM paciente WHERE IdPaciente=" + IdPaciente.getText().toString() + "");
+            if (rs.next()) {
+                nombre.setText(rs.getString(2));
+                telefono.setText(rs.getString(3));
+                correo.setText(rs.getString(4));
+                genero.setText(rs.getString(5));
+                pwd.setText(rs.getString(6));
+            }
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+/* //Codigo viejo
     //Metodo para registrar
     public void Registrar(View view) {
         AdminSQL admin = new AdminSQL(context; this, name; "administracion", factory; null, version; 1);
@@ -88,5 +162,5 @@ public class MainActivity extends AppCompatActivity {
         else {
             Toast.makeText(context; this, text;"Introduce la contraseña de usuario", Toast.LENGTH_SHORT).show();
         }
-    }
+    }*/
 }
